34ecfd07a8f93cabc949f906009e0e6e4b7784b4
gHainar <Dydykin.oleg2013@gmail.com>
2017-12-25 00:24:10 +0300

    Remove trash

--- SyntaxBuilder/builder_version.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define MAJOR_VERSION 0
-#define MINOR_VERSION 4
-#define SUFFIX_VERSION "alpha"
-#define PROGRAM_VERSION "0.4 alpha"
--- SyntaxBuilder/syntaxer_misc.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "lexer.h"
-
-syntaxer::syntax_node* delete_node(syntaxer::syntax_node* node);
-
-syntaxer::syntax_node* bend(syntaxer::syntax_node* node);
